c82c18c5e244d25856269425afdf990494f98f7f
kcampbell3190 <kcampbell3190@gmail.com>
2022-09-20 14:00:41 +1000

    updated functions code

--- main.py
+++ main.py
@@ -1,84 +1,50 @@
 # landing feature that will ask user what they wish to do within the app - Add a new meber, check for fines, check overdues, and check for available services.
-from dataclasses import dataclass
-# how do data types work in in loops etc
-#list of functions that landing page can loop through and execute
+
 features = ("create_new_member", "fine_check", "overdues", "check_for_available_services")
 choice = ""
 
-# feature 1 Library worker inputs information here for new members to the library 
 
-class LibraryMember():
-    def __init__(self, name, date_of_birth, fines,):
-        self.self = self
-        self.name = name
-        self.date_of_birth = date_of_birth #into a date type
-        self.fines = fines
-
-#list to store new members
-new_members = [
-     LibraryMember("Kane", 26_3_1993, 0 ),
-     LibraryMember("Chantal", 11_7_1994, 0),
-     LibraryMember("Jack", 22_11_2003, 0),
-
-
-
-def landing():
-    while True:
-        print("1) Create a new member")
-        print("2) Check for fines")
-        print("3) Check overdues")
-        print("3) Check for available services")
-        choice = input("Please select a number from the menu: ")
-        choice = choice.strip()
-
-
-    if (choice == "1"):
-        def create_new_member(): 
-            name = input("Please enter the name of the new member")
-        if input != "string":
-         print ("Invalid input: Please enter the name of the new member")
+ 
+ #Functions####################################################################
+ 
+def create_new_member(): 
+       #call class method
+        name = input("Please enter the name of the new member: ")
+        if name.isnumeric():
+            print ("Invalid input: Please enter the name of the new member")
+            name = input("Please enter the name of the new member: ")
         else:
-           
+          date_of_birth = input("Please enter the date of birth of the new member in the format YYYY-MM-DD")
+          if date_of_birth.isstring():
+            print("Invalid input: Please enter the DOB as a number")
+            date_of_birth = input("Please enter the DOB of the new member")
+          pass
+          name = self.self
+          name - name.self
+          date_of_birth = self.date_of_birth
+     
 
-        date_of_birth = print("Please enter the date of birth of the new member in the format YYYY-MM-DD")
-        x == input
-        if x != int():
-          print ("Invalid input: Please enter the DOB of the new member as numbers in the format YYYY-MM -DD. For                    example the 26th of March 1993 would be represented as26-03-1993")
-        else:
-          x
-
-
-
-
-# Feature 2 allows users to check for library fines
-        # How will we be able to check for other members
-        # cannot be a for loop
-    if (choice == "2"):
+def overdue():
+        num = int(input("how many days have you had the item? please enter a number between 1 and 14"))
+        if num.isstring():
+            print("Invalid input: Please enter a number between 1 and 14 as an integer")
+        if num <= 14:
+            result = (14 - num)
+        print (f"you have {result} days left of this item")
+        else: #num >= 14
+        return ("your item is overdue please return or renew the item")
         
-    def fine_check():
-        print("What is the member number?")
-          x = input
-    for fine in x:
-      if fine in x == 0
-         print("you do not have any fines")
-    else: 
-        print(f"you have a fine of {fine}")
+            
        
 
-# Feature 3 allows users to check how many days they have left until renewal or return
-    if (choice == "3"):
-        def overdue():
-            x = int(input("how many days have you had the item? please enter a number between 1 and 14"))
-        if isinstance(x, int):
-            print ("please put in the days represent as numbers such as one = 1") # this needs a rework
-            result = (x - 14)
-            return (f"you have {result} days left of this item")
 
 
 
-#Feature 4 this allows users to check what services are available and if they are free, bookable or cost
-    if (choice == "4"):
-        services_available {
+
+    #for items in services_available:
+def services_available():
+        
+        serviceslist {
     "scanning": "free",
     "printing": "costs per page",
     "borrowing": "free",
@@ -88,12 +54,94 @@ def landing():
     "local history": "free",
 }
 
-    for items in services_available:
-        print("Hi there what service are you looking for? Please input the name of the service otherwise input finished")
-        x = input
-    if input in service_avaible 
+        print("Hi there what service are you looking for?")
+        input = user_input("Enter the name of the service:")
+    if input in servicelist
         print (f"We have this {key} service and it is {value}")
     else: 
         print (" Apologise we do not have that service. Would you like to search for more?")
-    if x is "finished"
+    if input = "finished"
       print("Thank you please come again")
+
+            #############################################################################################################
+             
+            
+class LibraryMember():
+    # def __init__(self, name, date_of_birth, fines,):
+    def __init__(self, name, date_of_birth):
+        self.self = self
+        self.name = name
+        self.date_of_birth = date_of_birth #into a date type
+        # self.fines = fines
+        self.fines = 0
+    def check_fines():
+        print(f"you have a fine of ${self.fines}")
+ 
+        
+
+         #print("you do not have any fines")
+   # else: 
+       # print(f"you have a fine of {fine}")
+
+#list to store new members or do I want it stored in different variables 
+kane = LibraryMember1("Kane", 26_3_1993, 10 )
+
+new_members = [
+     kane,
+     LibraryMember("Chantal", 11_7_1994, 0),
+     LibraryMember("Jack", 22_11_2003, 0),
+]
+
+# landing feature that will ask user what they wish to do within the app - Add a new meber, check for fines, check overdues, and check for available services.
+
+def landing():
+   # while True:
+        print("1) Create a new member")
+        print("2) Check for fines")
+        print("3) Check overdues")
+        print("4) Check for available services")
+        print("5) Exit:")
+        # choice = input("Please select a number from the menu: ")
+       #  choice = choice.strip()
+
+while user_input != '5':
+
+    landing()
+    choice = int(input("Enter your choice"))
+
+   
+ # feature 1 Library worker inputs information here for new members to the library  
+    if (choice == 1):
+        create_new_member()
+       
+      
+
+
+
+
+# Feature 2 allows users to check for library fines
+        # How will we be able to check for other members
+        
+    if (choice == "2"):
+       fines() 
+    
+       
+
+# Feature 3 allows users to check how many days they have left until renewal or return
+    if (choice == "3"):
+         overdue()
+            
+
+
+
+#Feature 4 this allows users to check what services are available and if they are free, bookable or cost
+    if (choice == "4"):
+        services_available()
+    
+    
+    
+    
+    
+    if (choice == "5"):
+      print("Thank you please come again")
+      break
---
+++ readme.md
@@ -0,0 +1,25 @@
+##Documentation for T1A3 assignment
+
+
+
+
+#Installation instructions: 
+
+
+
+#Instructions: 
+
+The application will open with a menu so that the library staff member can decide which of the features they wish to use. 
+To initiate the feature the staff member will need to input the string that matches the feature. 
+Inside each if statement is a function that aligns to the feature. 
+Then to quit the program all the user needs to do is input quit which will exit the program. 
+
+Feature 1: To use this feature you will ne promted by the app to input data for each of the requirements of signing up a new member. 
+once tha information has been recorded the information will be stored as an instance of the LibraryMember class. 
+
+Feature 2: To use this feature you will be instructed to input the library members number. This will be the form of member1 etc. This will then loop through to the fines they might have and print out wether they have a fine and how much it is. If there is no fine the feature prints out "No fine". 
+
+Feature 3: This feature has been designed to check how many days until an item is due. You will be promted to enter the number of days that you have already had the item for. This will then subtract that number from the borrowing period of 14 days. The output will be a number stating how many days the library member has left on an item. If the item is over the 14 days the feature prints out "Your item is overdue please return or renew the item".
+
+Feature 4: To check if a service is available at a particular library you will be prompted to enter the service that is being looked for. The feature will then loop through the services avaialbe dictionary and inform the user if the services is available in an f statement that interlops the key and value from the dictionary. If the service is not available the feature prints out "That service is not available". 
+
